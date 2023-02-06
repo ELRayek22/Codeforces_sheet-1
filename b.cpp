c7d2deb33fe9f24147c332e85272a32cdf3b12e7
ELRayek22 <119718863+ELRayek22@users.noreply.github.com>
2023-02-06 21:54:53 +0200

    Create b.cpp

---
+++ b.cpp
@@ -0,0 +1,26 @@
+/*
+B. Basic Data Types
+Input
+Only one line containing the following space-separated values: int, long long, char, float and double respectively.
+
+Output
+Print each element on a new line in the same order it was received as input.
+
+Don't print any extra spaces.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A;
+    long long B;
+    char C;
+    float D;
+    double E;
+
+    cin>> A >> B >> C >> D >> E ;
+
+    cout<<A<<"\n"<<B<<"\n"<<C<<"\n"<<D<<"\n"<<E<<"\n";
+}
