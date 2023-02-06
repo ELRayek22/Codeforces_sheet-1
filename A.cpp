ea0d9fb63567293a9ba87f37062e4ab2da4990e7
ELRayek22 <119718863+ELRayek22@users.noreply.github.com>
2023-02-06 21:52:45 +0200

    Create A.cpp

---
+++ A.cpp
@@ -0,0 +1,20 @@
+/*A. Say Hello With C++
+Given a name S. Print "Hello, (name)" without parentheses.
+
+Input
+Only one line containing a string S.
+
+Output
+Print "Hello, " without quotes, then print name.
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string A;
+    cin>>A;
+
+    cout<<"Hello, "<< A ;
+}
