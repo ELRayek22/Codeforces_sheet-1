0f930f772d9f43c75dc42c43af3b93418c795aea
ELRayek22 <119718863+ELRayek22@users.noreply.github.com>
2023-02-25 15:45:42 +0200

    Create L.cpp

---
+++ L.cpp
@@ -0,0 +1,33 @@
+// solution 1
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string na1,na2;
+    string ma1,ma2;
+    cin>>na1 >>na2;
+    cin>>ma1 >>ma2;
+
+    if(na2 == ma2)
+        cout<<"ARE Brothers";
+    else
+        cout<<"NOT";
+}
+
+// solution 2
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string a,b,c,d;
+	cin>>a>>b>>c>>d;
+
+	(b==d)?
+
+	cout<<"ARE Brothers":cout<<"NOT"<<"\n";
+
+}
