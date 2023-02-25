390922725648a95c9478d0cef01fee160622a028
ELRayek22 <119718863+ELRayek22@users.noreply.github.com>
2023-02-25 15:18:35 +0200

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -1,12 +1,3 @@
-/*A. Say Hello With C++
-Given a name S. Print "Hello, (name)" without parentheses.
-
-Input
-Only one line containing a string S.
-
-Output
-Print "Hello, " without quotes, then print name.
-*/
 
 #include<iostream>
 using namespace std;
