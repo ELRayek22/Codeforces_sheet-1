7692c7164a3193509e9ec2846187444d2c569f7f
ELRayek22 <119718863+ELRayek22@users.noreply.github.com>
2023-02-25 15:19:43 +0200

    Update b.cpp

--- b.cpp
+++ b.cpp
@@ -1,14 +1,3 @@
-/*
-B. Basic Data Types
-Input
-Only one line containing the following space-separated values: int, long long, char, float and double respectively.
-
-Output
-Print each element on a new line in the same order it was received as input.
-
-Don't print any extra spaces.
-*/
-
 #include <iostream>
 using namespace std;
 
