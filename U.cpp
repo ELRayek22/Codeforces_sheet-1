384131424337dd5220d39c0b6d9c9ea161999542
ELRayek22 <119718863+ELRayek22@users.noreply.github.com>
2023-02-25 15:55:09 +0200

    Create U.cpp

---
+++ U.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	double d;
+	cin>>d;
+
+	if(ceil(d)==floor(d)) 
+    cout<<"int "<<int(d)<<endl;
+	else 
+    cout<<"float "<<int(d)<<" "<<d-int(d)<<endl;
+
+	return 0;
+}
